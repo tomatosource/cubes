bb5a58c353359eeab64c6680513533f5de962e2a
Geoff 👌 Cowan <geoffroy.cowan@gmail.com>
2023-01-28 17:02:09 +1100

    tunnel fly through

--- src/world.ts
+++ src/world.ts
@@ -7,7 +7,10 @@ import {
   Renderer,
   Mesh,
   MeshNormalMaterial,
+  Vector3,
 } from "three";
+
+const CAMERA_ROTATE_RADIUS = 30;
 
 export class World {
   scene: Scene;
@@ -28,19 +31,13 @@ export class World {
       1, // near fulcrum, i.e. min draw distance
       1000 // far fulcrum, i.e. max draw distance
     );
-    this.camera.position.set(0, 0, 8);
+    this.camera.position.set(CAMERA_ROTATE_RADIUS, 0, 0);
     this.camera.lookAt(this.scene.position);
 
     this.renderer = new WebGLRenderer({ antialias: true });
     this.renderer.setSize(windowWidth, windowHeight);
 
-    const cubeCount = 2;
-    this.cubes = [];
-    for (let i = 0; i < cubeCount; i++) {
-      const cube = new Mesh(new BoxGeometry(2, 2, 2), new MeshNormalMaterial());
-      this.scene.add(cube);
-      this.cubes.push(cube);
-    }
+    this.cubes = this.initCubes();
 
     this.logger = document.getElementById("log");
     this.log("hello world");
@@ -51,17 +48,60 @@ export class World {
     if (content.length > 5000) {
       content = content.slice(1000);
     }
-    this.logger.innerHTML =
-      `[${new Date().toLocaleTimeString()}] ${msg}<br/>` + content;
+    this.logger.innerHTML = `[${new Date().toLocaleTimeString()}] ${msg}<br/>${content}`;
+  }
+
+  initCubes(): Array<Mesh> {
+    const cubes = [];
+    const tubeSteps = 50;
+    const stepSize = 0.8;
+    const ringCount = 20;
+    const tubeRadius = 3;
+    for (let i = 0; i < tubeSteps; i++) {
+      for (let j = 0; j < ringCount; j++) {
+        const cube = new Mesh(new BoxGeometry(), new MeshNormalMaterial());
+
+        const y = Math.sin(Math.PI * 2 * (j / ringCount)) * tubeRadius;
+        const z = Math.cos(Math.PI * 2 * (j / ringCount)) * tubeRadius;
+
+        const offset = new Vector3(
+          i * stepSize - (stepSize * tubeSteps) / 2,
+          y,
+          z
+        );
+        cube.position.add(offset);
+        cube.rotation.x = (Math.PI / 2) * Math.random();
+        cube.rotation.z = (Math.PI / 2) * Math.random();
+        this.scene.add(cube);
+        cubes.push(cube);
+      }
+    }
+    return cubes;
   }
 
   update(delta: number) {
-    const speed = 0.0003 * delta;
-    for (let i = 0; i < this.cubes.length; i++) {
-      const mod = i + 1;
-      this.cubes[i].rotation.x -= speed * mod;
-      this.cubes[i].rotation.y -= speed * mod;
-      this.cubes[i].rotation.z -= speed * mod;
-    }
+    const cubeSpinSpeed = 0.0001 * delta;
+    const trackSpeed = 0.0015 * delta;
+    const camRPM = 0.12;
+
+    // for (let i = 0; i < this.cubes.length; i++) {
+    // // if (i % 3 == 0) {
+    // // continue;
+    // // }
+    // const mod = (Math.random() * i) / 5;
+    // this.cubes[i].rotation.x -= cubeSpinSpeed * mod;
+    // this.cubes[i].rotation.y -= cubeSpinSpeed * mod;
+    // this.cubes[i].rotation.z -= cubeSpinSpeed * mod;
+    // }
+    const { x } = this.camera.position;
+    // const theta = Math.atan2(x, z);
+    // const newTheta = theta + (delta * Math.PI * camRPM) / 1000;
+    // const newX = Math.sin(newTheta) * CAMERA_ROTATE_RADIUS;
+    // const newZ = Math.cos(newTheta) * CAMERA_ROTATE_RADIUS;
+
+    this.camera.position.set(x - trackSpeed, 0, 0);
+    // this.camera.lookAt(this.scene.position);
+    // this.camera.rotateZ(0.0001 * delta);
+    this.log((1000 / delta).toFixed(2));
   }
 }
